de97a0a5c2e69c24ad6be0e2847b45e496b6482f
Oliver Loe <oliver.loe@gmail.com>
2016-03-09 19:31:14 +0200

    Added example for POJO

--- springamqp/rpc/src/main/java/com/seedollar/java/springamqp/RPCRabbitMQMain.java
+++ springamqp/rpc/src/main/java/com/seedollar/java/springamqp/RPCRabbitMQMain.java
@@ -1,6 +1,7 @@
 package com.seedollar.java.springamqp;
 
 import com.seedollar.java.springamqp.configuration.QueueConfiguration;
+import com.seedollar.java.springamqp.model.Person;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
@@ -9,6 +10,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+
+import java.util.Date;
 
 /**
  * Created by seedollar on 2016/03/09.
@@ -27,13 +30,22 @@ public class RPCRabbitMQMain implements ApplicationRunner {
         SpringApplication application = new SpringApplication(RPCRabbitMQMain.class);
         application.setApplicationContextClass(AnnotationConfigApplicationContext.class);
         application.run(args);
-
     }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println("invoking RPC......");
-        Object response = rabbitTemplate.convertSendAndReceive(queueConfiguration.rpcDefaultExchange().getName(), "rpc", "test");
-        System.out.println("response = " + response);
+        System.out.println("invoking RPC via convertSendAndReceive() with primitive");
+        Object responsePrimitive = rabbitTemplate.convertSendAndReceive(queueConfiguration.rpcDefaultExchange().getName(), "rpc", "test");
+        System.out.println("response = " + responsePrimitive);
+
+        System.out.println("invoking RPC via convertSendAndReceive() with POJO");
+        Person message = new Person();
+        message.setDateOfBirth(new Date());
+        message.setFirstName("Jared");
+        message.setFirstName("Leto");
+        Object responsePojo = rabbitTemplate.convertSendAndReceive(queueConfiguration.rpcDefaultExchange().getName(), "rpc", message);
+        System.out.println("response = " + responsePojo);
+
+        System.exit(0);
     }
 }
--- springamqp/rpc/src/main/java/com/seedollar/java/springamqp/configuration/RabbitListenerConfiguration.java
+++ springamqp/rpc/src/main/java/com/seedollar/java/springamqp/configuration/RabbitListenerConfiguration.java
@@ -1,5 +1,6 @@
 package com.seedollar.java.springamqp.configuration;
 
+import com.seedollar.java.springamqp.model.Person;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.context.annotation.Configuration;
@@ -15,7 +16,16 @@ public class RabbitListenerConfiguration {
 
     @RabbitListener(containerFactory = "simpleRabbitListenerContainerFactory", queues = "rpc_default_queue")
     public String rpc(String request) throws InterruptedException {
+        System.out.println("Replying to primitive request");
         Thread.sleep(2000);
         return "rpc response";
+    }
+
+    @RabbitListener(containerFactory = "simpleRabbitListenerContainerFactory", queues = "rpc_default_queue")
+    public Person rpcPojo(Person request) throws InterruptedException {
+        System.out.println("Replying to POJO request");
+        Thread.sleep(2000);
+        request.setLastName("Mazini");
+        return request;
     }
 }
